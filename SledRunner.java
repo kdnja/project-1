126e6858553c191dde369e59f2d311b640ce8074
Caden Jamason <me@kdn.pw>
2022-05-13 13:23:08 -0700

    Added toboggan test cases, organized them

--- SledRunner.java
+++ SledRunner.java
@@ -2,9 +2,18 @@ public class SledRunner {
     public static void main(String[] args) {
         RectangularSled test = new RectangularSled();
         System.out.println(test.getInfo());
+        System.out.println(test.getRawInfo());
+
         CircularSled test1 = new CircularSled();
         System.out.println(test1.getInfo());
         test1.setColor("yellow");
         System.out.println(test1.getInfo());
+        System.out.println(test1.getRawInfo());
+
+        Toboggan toboggan = new Toboggan();
+        System.out.println(toboggan.getInfo());
+        toboggan.setTraditionalShape(false);
+        System.out.println(toboggan.getInfo());
+        System.out.println(toboggan.getRawInfo());
     }
 }
